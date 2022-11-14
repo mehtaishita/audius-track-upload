b61b537a1f030ca38c3df54fbe53a99edd096510
mehtaishita <ishita.mehta33@outlook.com>
2022-11-14 16:26:30 -0600

    upload track rpc

--- programs/audius-track-upload/src/lib.rs
+++ programs/audius-track-upload/src/lib.rs
@@ -6,7 +6,15 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod audius_track_upload {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> { 
+        Ok(())
+    }
+
+    pub fn upload_track<'info>(ctx: Context<TrackLibrary>, cid: String) -> Result<()> {
+        // TODO: if (no all tracks no init) let mut all_tracks = HashMap::new();
+        let track_id = Track::new_track_id();
+        let new_track = Track { cid };
+        ctx.accounts.all_tracks.insert(track_id,new_track);
         Ok(())
     }
 }
